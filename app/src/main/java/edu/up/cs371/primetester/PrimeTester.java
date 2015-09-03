dfc8620e702985742ccdf5f89e99e46399267ecb
thomasb18 <bmtmail@comcast.net>
2015-09-03 16:05:47 -0700

    Prime testing algorithm attempt 1.

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -11,10 +11,22 @@ public class PrimeTester {
      * Tells whether an integer is prime.
      *
      * @param n the number to test
-     * @return true iff n is prime
+     * @return true if n is prime
      */
     public static boolean isPrime(long n) {
-        // for now, return a random result
-        return Math.random() > 0.5;
+        if(n == 2){
+            return true;
+        }
+        else if (n % 2 == 0){
+            return false;
+        }
+        else {
+            for (int i = 3; i < n / 3; ++i){
+                if(n % i == 0){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
